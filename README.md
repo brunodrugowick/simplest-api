7e78942d26eacc40cba099b5a967f372e233de62
brunodrugowick <bruno.drugowick@gmail.com>
2019-03-04 03:46:18 -0300

    Adding Lombok and simplifying the operation.
    * Adding Lombok and simplifying the operation.
    
    Adds Lombok to configure a Response object without much code (although I still had to build the constructors I wanted) and also simplifies the API to hava a unique endpoint.
    
    Also adds @RestController, which eliminates the need for a @ResponseBody on each method.

--- README.md
+++ README.md
@@ -8,13 +8,23 @@ Clone or download it and:
 ```
 
 ## Using it
-You should access or call `http://localhost:8080`. You can append the following to make operations:
+You should access or call `http://localhost:8080/calc`. You must append the following to make operations:
 
-* `/sum`
-* `/subtract`
-* `/divide`
-* `/multiply`
+* A mandatory parameter `a`
+* A mandatory parameter `b`
+* A optional parameter `operation` with one of the following values:
+    * `sum` (default)
+    * `subtract`
+    * `divide`
+    * `multiply`
 
-All operations take two integers `a` and `b` and return an integer as a result of the operation.
-
-For example, if you call [http://localhost:8080/subtract?a=5&b=15](http://localhost:8080/subtract?a=5&b=15) you get `-10` as a response right on your browser.
+For example, if you call [http://localhost:8080/calc?a=5&b=15&operation=subtract](http://localhost:8080/calc?a=5&b=15&operation=subtract) on your browser, you get the following JSON as a response:
+```json
+{
+  "a": 5,
+  "operation": "subtract",
+  "b": 15,
+  "result": -10,
+  "error": null
+}
+```
--- src/main/java/com/drugowick/simplestapi/controller/CalculatorController.java
+++ src/main/java/com/drugowick/simplestapi/controller/CalculatorController.java
@@ -1,30 +1,48 @@
 package com.drugowick.simplestapi.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.Data;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CalculatorController {
 
-    @RequestMapping("/sum")
-    public @ResponseBody int sum(@RequestParam int a, @RequestParam int b) {
-        return a+b;
+    /**
+     * A Response inner class to provide the response.
+     */
+    @Data
+    public class Response {
+        private int a;
+        private String operation;
+        private int b;
+        private int result;
+        private String error;
+
+        public Response(String error) {
+            this.error = error;
+        }
+
+        public Response(int a, String operation, int b, int result) {
+            this.a = a;
+            this.operation = operation;
+            this.b = b;
+            this.result = result;
+        }
     }
 
-    @RequestMapping("/subtract")
-    public @ResponseBody int subtract(@RequestParam int a, @RequestParam int b) {
-        return a-b;
-    }
-
-    @RequestMapping("/divide")
-    public @ResponseBody int divide(@RequestParam int a, @RequestParam int b) {
-        return a/b;
-    }
-
-    @RequestMapping("/multiply")
-    public @ResponseBody int multiply(@RequestParam int a, @RequestParam int b) {
-        return a*b;
+    @RequestMapping("/calc")
+    public Response calc(
+            @RequestParam int a,
+            @RequestParam int b,
+            @RequestParam(required = false, defaultValue = "sum") String operation)
+    {
+        switch (operation) {
+            case "sum": return new Response(a, "sum", b, a+b);
+            case "subtract": return new Response(a, "subtract", b, a-b);
+            case "multiply": return new Response(a, "multiply", b, a*b);
+            case "divide": return new Response(a, "divide", b, a/b);
+            default: return new Response("Valid operations are sum (default), subtract, multiply or divide.");
+        }
     }
 }
